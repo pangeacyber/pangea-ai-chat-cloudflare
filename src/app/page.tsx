9eb333fdd7765d7518c7caa7102925b0fa87ddf0
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2024-12-13 19:09:55 -0500

    Refactor env var loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,27 +4,24 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import { AuthProvider } from "@pangeacyber/react-auth";
 
+import { env } from "@src/env";
+
 import { ChatProvider } from "./context";
 import Layout from "./features/Layout";
 import PangeaDark from "./theme";
-
-const LOGIN_URL = process.env.NEXT_PUBLIC_AUTHN_UI_URL || "";
-const CLIENT_TOKEN = process.env.NEXT_PUBLIC_PANGEA_CLIENT_TOKEN || "";
-const PANGEA_DOMAIN =
-  process.env.NEXT_PUBLIC_PANGEA_BASE_DOMAIN || "aws.us.pangea.cloud";
 
 export default function Home() {
   return (
     <AuthProvider
       config={{
-        domain: PANGEA_DOMAIN,
-        clientToken: CLIENT_TOKEN,
+        domain: env.NEXT_PUBLIC_PANGEA_BASE_DOMAIN,
+        clientToken: env.NEXT_PUBLIC_PANGEA_CLIENT_TOKEN,
       }}
       cookieOptions={{
         useCookie: true,
       }}
       // onLogin={handleLogin}
-      loginUrl={LOGIN_URL}
+      loginUrl={env.NEXT_PUBLIC_AUTHN_UI_URL}
       useStrictStateCheck={false}
     >
       <ThemeProvider theme={PangeaDark}>
