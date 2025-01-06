390bc51f27008f115f0b4388d625d1dc32723162
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2025-01-06 13:43:12 -0500

    Prepare for new AI Guard response schema
    The response body of AI Guard's API is changing significantly, so this patch
    prepares the app for this by looking for both response schemas when parsing
    findings.

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import type { AIGuard } from "pangea-node-sdk";
+
 export interface PangeaResponse<T = unknown> {
   request_id: string;
   request_time: string;
@@ -6,3 +8,26 @@ export interface PangeaResponse<T = unknown> {
   summary: string;
   result: T;
 }
+
+export type AIGuardResultV1 = AIGuard.TextGuardResult;
+
+export interface AIGuardDetector<T> {
+  detected: boolean;
+  data: T | null;
+}
+export interface AIGuardResultV2 {
+  detectors: {
+    prompt_injection: AIGuardDetector<{
+      analyzer_responses: { analyzer: string; confidence: number }[];
+    }>;
+    pii_entity?: AIGuardDetector<{
+      entities: { redacted: boolean }[];
+    }>;
+    malicious_entity?: AIGuardDetector<{
+      entities: unknown[];
+    }>;
+  };
+  prompt: string;
+}
+
+export type AIGuardResult = AIGuardResultV1 | AIGuardResultV2;
